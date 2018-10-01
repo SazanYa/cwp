cb4dabda2e6e03c236f8cce106729013d653c06d
Sazanovets Yaroslav <sazanovec.yaroslav@mail.ru>
2018-10-01 09:23:05 +0300

    cwp-02 v1.0

--- labs/cwp-02/client-swarm.js
+++ labs/cwp-02/client-swarm.js
@@ -1,5 +1,9 @@
 const child_process = require('child_process');
 
+if (process.argv.length !== 3) {
+    process.exit(-1);
+}
+
 for (let i = 0; i < process.argv[2]; i++) {
     const worker = child_process.exec('node client.js', function(err, stdout, stderr) {
         if (err) {
